fce3a20d397ccbe1a056a458d80b81c8782d585e
Belen Castelli <mariabelencastelli29@gmail.com>
2024-05-16 17:49:52 +0200

    Contact

--- portfolio/src/app/shared/proyects.service.ts
+++ portfolio/src/app/shared/proyects.service.ts
@@ -7,14 +7,14 @@ import { Project } from '../models/project';
 export class ProyectsService {
   public projects: Project[]
   constructor() { 
-    this.projects = [new Project('myBooks','Aplicación web para gestionar tu biblioteca personal. Pudiendo añadir, modificar y eliminar libros.', 
-    'Desarrollada en Angular y respaldada por una base de datos relacional (MySQL).', 'https://i.imgur.com/7MS3VuB.png'),
-    new Project('PokeApi', 'Aplicación web para encontrar tus Pokémon favoritos. Puedes buscar por nombre, ID o tipo',
-      'Desarrollada con HTML, CSS y JavaScript, esta aplicación se integra con  la API externa PokéApi para obtener datos actualizados sobre Pokémon.',
-      'https://i.imgur.com/aXRGYxy.png'
+    this.projects = [new Project('myBooks','Aplicación web para gestionar tu biblioteca personal.', 
+    'Desarrollada en Angular y respaldada por una base de datos relacional (MySQL).', 'https://i.imgur.com/7MS3VuB.png', 'https://github.com/BelenCastelli/appBooks'),
+    new Project('PokeApi', 'Aplicación web para encontrar pokemons. Puedes buscar por nombre, ID o tipo.',
+      'Desarrollada con HTML, CSS y JavaScript, e integra la API externa PokéApi.',
+      'https://i.imgur.com/aXRGYxy.png', 'https://github.com/BelenCastelli/PokeApi'
     ),
     new Project('Tres en raya', 'Juego de tres en raya para dos jugadores.', 'Desarrollado con HTML, CSS y JavaScript.',
-      'https://i.imgur.com/AqikaFQ.png'
+      'https://i.imgur.com/AqikaFQ.png', 'https://github.com/BelenCastelli/TresEnRaya'
     )
   ]
   }
