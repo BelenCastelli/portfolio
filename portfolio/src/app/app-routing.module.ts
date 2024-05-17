a96aadcac4651417cab92bf7aee5c9eff0d15e3c
Belen Castelli <mariabelencastelli29@gmail.com>
2024-05-17 13:48:12 +0200

    navigation

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TechsComponent } from './pages/techs/techs.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   {path: '', component :HomeComponent},
+  {path: 'home', component :HomeComponent},
+  {path: 'about', component :AboutComponent},
+  {path: 'techs', component :TechsComponent},
+  {path: 'projects', component :ProyectosComponent},
+  {path: 'contact', component :ContactComponent}
 ];
+const routerOption: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled'
 
+}
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOption)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
